5e59f0c8e8fa9f5c610549fd748b28ab2c0c3f68
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-18 16:55:13 -0300

    header container

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="container">
+        
+        </div>
+    );
+}
+
+export default Header;
