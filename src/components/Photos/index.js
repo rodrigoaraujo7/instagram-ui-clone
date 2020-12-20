b4f0c1bab5df56bd56edf7826d149b5694ab2a85
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-20 17:12:32 -0300

    Photos Grid Layout

---
+++ src/components/Photos/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Photos.css'
+
+const Photos = () => {
+    return (
+        <center>
+            <div className="container">
+                <div className="grip-wrapper">
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                </div>
+            </div>
+        </center>
+    )
+}
+
+export default Photos
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/Header';
 import AccountInfos from './components/AccountInfos';
+import Photos from  './components/Photos';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <AccountInfos />
+    <hr />
+    <Photos />
   </React.StrictMode>,
   document.getElementById('root')
 );
