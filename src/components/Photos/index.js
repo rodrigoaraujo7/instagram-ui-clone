af36e30c37b2852bb7bb828d7a53009fd2f8e2ea
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-22 20:33:20 -0300

    900px 2 columns

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Photos.css'
 
 const Photos = () => {
-    const photo = [
+    const [photosArray] = useState ([
         { src: '', likes: 47 },
         { src: '', likes: 27 },
         { src: '', likes: 151 },
         { src: '', likes: 85 },
         { src: '', likes: 13 }
-    ]
+    ])
 
     return (
         <center>
             <div className="container">
                 <div className="grid-wrapper">
-                    {photo.map(photo => (
+                    {photosArray.map(photosArray => (
                         <div className="grid-item">
-                            <img src={photo.src} />
+                            <img src={photosArray.src} />
                         </div>
-                    ))
-                    }
+                    ))}
                 </div>
             </div>
         </center>
