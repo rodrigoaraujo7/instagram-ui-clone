523a88f2f67273f43103efad5e3e0aa17ef06c0a
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-22 03:24:00 -0300

    useState

--- src/components/AccountInfos/index.js
+++ src/components/AccountInfos/index.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './AccountInfos.css'
 
 const AccountInfos = () => {
     const informations = {
         userPhoto: '',
         user: 'rodrigoaraujo7',
-        publications: 15,
-        followers: 576
+        publications: 15
     }
+
+    const [follow, setFollow] = useState(576);
 
     return (
         <div className="infos--center">
@@ -18,7 +19,7 @@ const AccountInfos = () => {
 
                 <div className="infos--text">
                     <h1 className="infos--username">{informations.user}</h1>
-                    <input type="submit" value="Seguir" />
+                    <input type="submit" value="Follow" onClick={() => setFollow(follow + 1)} />
                     <div className="infos--flex">
                         <div className="infos--publications">
                             <h2>{informations.publications}</h2>
@@ -26,7 +27,7 @@ const AccountInfos = () => {
                         </div>
 
                         <div className="infos--followers">
-                            <h2>{informations.followers}</h2>
+                            <h2>{follow}</h2>
                             <h2>followers</h2>
                         </div>
                     </div>
