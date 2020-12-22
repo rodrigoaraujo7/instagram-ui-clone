6ec9c6bb5a3fea1bc9e9425afece2bf9ef33e667
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-22 01:45:33 -0300

    user informations in a variable

--- src/components/AccountInfos/index.js
+++ src/components/AccountInfos/index.js
@@ -2,23 +2,30 @@ import React from 'react';
 import './AccountInfos.css'
 
 const AccountInfos = () => {
+    const informations = {
+        userPhoto: '',
+        user: 'rodrigoaraujo7',
+        publications: 15,
+        followers: 576
+    }
+
     return (
         <div className="infos--center">
             <div className="infos--container">
                 <div className="infos--photo">
-                    <img />
+                    <img src={informations.userPhoto} />
                 </div>
 
                 <div className="infos--text">
-                    <h1 className="infos--username">rodrigoaraujo7</h1>
+                    <h1 className="infos--username">{informations.user}</h1>
                     <div className="infos--flex">
                         <div className="infos--publications">
-                            <h2>15</h2>
+                            <h2>{informations.publications}</h2>
                             <h2>publications</h2>
                         </div>
 
                         <div className="infos--followers">
-                            <h2>576</h2>
+                            <h2>{informations.followers}</h2>
                             <h2>followers</h2>
                         </div>
                     </div>
