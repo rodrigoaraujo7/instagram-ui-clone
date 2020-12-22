a884b10bb9fff7c3c0fcdd73f5a80a8f750a7d08
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-22 19:44:18 -0300

    200x200 UserPhoto

--- src/components/AccountInfos/index.js
+++ src/components/AccountInfos/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import './AccountInfos.css'
+import './AccountInfos.css';
+import UserPhoto200px from '../public/UserPhoto.png';
 
 const AccountInfos = () => {
     const informations = {
-        userPhoto: '',
+        userPhoto: UserPhoto200px,
         user: 'rodrigoaraujo7',
         publications: 15,
         followers: 576
