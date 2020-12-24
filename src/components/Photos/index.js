891c2c5c2b098b6f210e355e8348da8989c7b684
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-12-23 23:53:32 -0300

    New Images

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import './Photos.css'
 
+import Photo1 from '../public/photo1.jpg';
+import Photo2 from '../public/photo2.jpg';
+import Photo3 from '../public/photo3.jpg';
+import Photo4 from '../public/photo4.jpg';
+import Photo5 from '../public/photo5.jpg';
+
 const Photos = () => {
-    const [photosArray] = useState ([
-        { src: '', likes: 47 },
-        { src: '', likes: 27 },
-        { src: '', likes: 151 },
-        { src: '', likes: 85 },
-        { src: '', likes: 13 }
+    const [photosArray] = useState([
+        { src: Photo1, likes: 47 },
+        { src: Photo2, likes: 27 },
+        { src: Photo3, likes: 151 },
+        { src: Photo4, likes: 85 },
+        { src: Photo5, likes: 13 }
     ])
 
     return (
